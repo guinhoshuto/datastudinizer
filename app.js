f4782aa7349a5f339625aa81cc2cf1cfbf89904d
Guilherme S <guinho.shuto@gmail.com>
2018-08-24 23:33:16 -0400

    config token

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const config = require('./config');
 const app = express();
 
+app.get('/', function(req, res){
+    res.status(200).json({
+        message: 'oi'
+    })
+})
+
+
+
+app.listen(3000, function(){
+    console.log('oioi');
+    console.log(config.token);
+})
 module.exports = app;
 
---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    token: 'EAAMDMdKeymIBAERAlYixaso24rWlQhdBZAE4zVhe7u8ZCfZAOrkZCPy3Nad9dAsfzQuHq5apnHuWjZBgJV1YZBAgIrm0X27AqUMEb0w01kaZCQPQV6HRY99pyg4LI3Vgu6yvjjxH3UuBYOsMytUsPraQca5RDUO8HOKQRXYpwWJ0gZDZD'
+}
+
+module.exports = config;
