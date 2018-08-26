d5197c555090345fe7b029c87eb47bc54e56d440
Guilherme S <guinho.shuto@gmail.com>
2018-08-25 23:43:04 -0400

    primeira rota

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const config = require('./config');
 const app = express();
 
+const fbAds = require('./facebook/ads');
+
+app.use('/facebook/ads', fbAds);
+
 app.get('/', function(req, res){
     res.status(200).json({
         message: 'oi'
