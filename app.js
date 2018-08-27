575380a7a80e6290875c2ba1bbacae13b4945831
Guilherme S <guinho.shuto@gmail.com>
2018-08-26 23:22:54 -0400

    Mais duas rotas

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const config = require('./config');
 const app = express();
 
 const fbAds = require('./facebook/ads');
+const igAds = require('./instagram/ads');
+const stories = require('./instagram/stories');
 
 app.use('/facebook/ads', fbAds);
+app.use('/instagram/ads', igAds);
+app.use('/instagram/stories', stories);
 
 app.get('/', function(req, res){
     res.status(200).json({
